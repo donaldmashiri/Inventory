44c18dbaab38e67d50f03971d6075e9a13e9358e
Donald Mashiri <mashirid@staff.msu.ac.zw>
2024-05-27 17:51:09 +0200

    h

---
+++ database/migrations/2024_05_27_154110_create_inventories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_name');
+            $table->text('item_description');
+            $table->string('item_category');
+            $table->string('item_code');
+            $table->integer('quantity');
+            $table->integer('reorder_level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventories');
+    }
+};
