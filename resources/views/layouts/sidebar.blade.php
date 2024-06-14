c91bc6eff94632a141b59671417f344b23bfbc8a
Donald Mashiri <dondaldtondemashiri@gmail.com>
2024-06-14 21:58:11 +0200

    complete code

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,41 +8,70 @@
                         <i class="bi bi-person"></i> Profile
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('users.index') }}">
-                        <i class="bi bi-people"></i> User and Access Management
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('suppliers.index') }}">
-                        <i class="bi bi-truck"></i> Suppliers
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('orders.index') }}">
-                        <i class="bi bi-clipboard-data"></i> Purchase Orders
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('inventories.index') }}">
-                        <i class="bi bi-box"></i> Inventory
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('requisitions.index') }}">
-                        <i class="bi bi-clipboard-check"></i> Requisitions
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('invoices.index') }}">
-                        <i class="bi bi-file-earmark-text"></i> Invoices
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('reports.index') }}">
-                        <i class="bi bi-bar-chart"></i> Reporting Analytics
-                    </a>
-                </li>
+                @if(auth()->user()->role == 'Inventory Clerk')
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('suppliers.index') }}">
+                            <i class="bi bi-truck"></i> Suppliers
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('inventories.index') }}">
+                            <i class="bi bi-box"></i> Inventory
+                        </a>
+                    </li>
+                @elseif(auth()->user()->role == 'Accounting Manager')
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('requisitions.index') }}">
+                            <i class="bi bi-clipboard-check"></i> Requisitions
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('invoices.index') }}">
+                            <i class="bi bi-file-earmark-text"></i> Invoices
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('orders.index') }}">
+                            <i class="bi bi-clipboard-data"></i> Purchase Orders
+                        </a>
+                    </li>
+                @elseif(auth()->user()->role == 'Procurement Manager')
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('users.index') }}">
+                            <i class="bi bi-people"></i> User and Access Management
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('suppliers.index') }}">
+                            <i class="bi bi-truck"></i> Suppliers
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('orders.index') }}">
+                            <i class="bi bi-clipboard-data"></i> Purchase Orders
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('inventories.index') }}">
+                            <i class="bi bi-box"></i> Inventory
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('requisitions.index') }}">
+                            <i class="bi bi-clipboard-check"></i> Requisitions
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('invoices.index') }}">
+                            <i class="bi bi-file-earmark-text"></i> Invoices
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('reports.index') }}">
+                            <i class="bi bi-bar-chart"></i> Reporting Analytics
+                        </a>
+                    </li>
+                @endif
             </ul>
         </div>
     </div>
